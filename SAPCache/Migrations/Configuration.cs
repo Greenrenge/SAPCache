4db0da7369b920f8ec81dd8d3e40d93d54b8a407
Sorasak Srirussamee(MALEE) <Sorasak_Sri@malee.co.th>
2018-01-18 14:14:51 +0700

    view of customer detail

--- SAPCache/Migrations/Configuration.cs
+++ SAPCache/Migrations/Configuration.cs
@@ -110,7 +110,103 @@ namespace SAPCache.Migrations
                                         END
 ");
 
+            context.Database.ExecuteSqlCommand(@"
+            IF OBJECT_ID('dbo.VIEW_KNVV_TEXT') IS NULL
+                                        BEGIN
+                                            EXECUTE('CREATE VIEW dbo.VIEW_CUSTOMER_DETAIL_MASTER
+                                            AS
+                                                select 	
+	dbo.unwrapzero(kna1.KUNNR) as customercode
+	,adrc.[NAME1] as name1
+	,adrc.[NAME2] as name2
+	,adrc.[NAME4] as branch_name
+	,adrc.[SORT1] as search_term1
+	,adrc.[SORT2] as search_term2
+	,adrc.[STR_SUPPL1] as street2
+	,adrc.[STR_SUPPL2] as street3
+	,adrc.[STR_SUPPL3] as street4
+	,adrc.[LOCATION] as street5
+	,adrc.[CITY1] as city
+	,adrc.[CITY2] as district
+	,adrc.[REGION] as region
+	,adrc.[COUNTRY] as country
+	,adrc.[POST_CODE1] as postal_code
+	,adrc.[TRANSPZONE] as transport_zone1
+	,adrc.[TEL_NUMBER] as tel_no
+	,adrc.[TEL_EXTENS] as tel_ext
+	,adrc.[FAX_NUMBER] as fax_no
+	,adrc.[FAX_EXTENS] as fax_ext
+	,adr2.TEL_NUMBER as mobile_no
+	,adr6.SMTP_ADDR as email
+	,kna1.KTOKD as account_group
+	,kna1.STCD1 as tax_number1
+	,kna1.STCD3 as tax_number3
+	,kna1.STCEG as vat_register_number
+	,kna1.LZONE as transport_zone2
+	,kna1.VBUND as trading_partner
+	,kna1.AUFSD as kna1_order_block
+	,kna1.FAKSD as kna1_bill_block
+	,kna1.LIFSD as kna1_delivery_block
+	,kna1.CASSD as kna1_sales_block
+	
+from dbo.KNA1 kna1	
+left join dbo.ADRC adrc on kna1.ADRNR = adrc.[ADDRNUMBER] and adrc.NATION <> ''I''
+left join dbo.ADR6 adr6 on kna1.ADRNR = adr6.[ADDRNUMBER] and LEN(adr6.PERSNUMBER)=0 and adr6.FLGDEFAULT = ''X''
+left join dbo.ADR2 adr2 on kna1.ADRNR = adr2.[ADDRNUMBER] and LEN(adr2.PERSNUMBER)=0 and adr2.FLGDEFAULT = ''X''
 
+                                        
+                                        
+                                        
+                                        
+                                        END
+                                        ELSE
+                                        BEGIN
+                                            EXECUTE(''ALTER VIEW dbo.VIEW_KNVV_TEXT
+                                            AS
+                                                select 	
+	dbo.unwrapzero(kna1.KUNNR) as customercode
+	,adrc.[NAME1] as name1
+	,adrc.[NAME2] as name2
+	,adrc.[NAME4] as branch_name
+	,adrc.[SORT1] as search_term1
+	,adrc.[SORT2] as search_term2
+	,adrc.[STR_SUPPL1] as street2
+	,adrc.[STR_SUPPL2] as street3
+	,adrc.[STR_SUPPL3] as street4
+	,adrc.[LOCATION] as street5
+	,adrc.[CITY1] as city
+	,adrc.[CITY2] as district
+	,adrc.[REGION] as region
+	,adrc.[COUNTRY] as country
+	,adrc.[POST_CODE1] as postal_code
+	,adrc.[TRANSPZONE] as transport_zone1
+	,adrc.[TEL_NUMBER] as tel_no
+	,adrc.[TEL_EXTENS] as tel_ext
+	,adrc.[FAX_NUMBER] as fax_no
+	,adrc.[FAX_EXTENS] as fax_ext
+	,adr2.TEL_NUMBER as mobile_no
+	,adr6.SMTP_ADDR as email
+	,kna1.KTOKD as account_group
+	,kna1.STCD1 as tax_number1
+	,kna1.STCD3 as tax_number3
+	,kna1.STCEG as vat_register_number
+	,kna1.LZONE as transport_zone2
+	,kna1.VBUND as trading_partner
+	,kna1.AUFSD as kna1_order_block
+	,kna1.FAKSD as kna1_bill_block
+	,kna1.LIFSD as kna1_delivery_block
+	,kna1.CASSD as kna1_sales_block
+	
+from dbo.KNA1 kna1	
+left join dbo.ADRC adrc on kna1.ADRNR = adrc.[ADDRNUMBER] and adrc.NATION <> ''I''
+left join dbo.ADR6 adr6 on kna1.ADRNR = adr6.[ADDRNUMBER] and LEN(adr6.PERSNUMBER)=0 and adr6.FLGDEFAULT = ''X''
+left join dbo.ADR2 adr2 on kna1.ADRNR = adr2.[ADDRNUMBER] and LEN(adr2.PERSNUMBER)=0 and adr2.FLGDEFAULT = ''X''
+')
+                                        
+                                        
+                                        
+                                       
+                                        END");
 
             //this is for cutting zero out of the the begining of the string 
             context.Database.ExecuteSqlCommand(@"
